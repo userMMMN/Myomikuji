f3c62db6da22815f3040b87aaa0740f4755339bd
Hayato <5saphone92@gmail.com>
2019-12-18 23:18:32 +0900

    commit

---
+++ PHP/laravel_blog/myblog/app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+      if (\DB::getDriverName() === 'sqlite') {
+        \DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
+      }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
